622536d9463fbc80bcc95d1a6fc191825e9bf2ce
Bukola <bukolaebikefe@yahoo.com>
2022-02-11 11:29:06 +0100

    setup axios and fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { useEffect } from "react";
-import "./App.css";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { updateData } from "./store/coronaData/data";
+import axios from "axios";
+
+const baseURL = "https://covidnigeria.herokuapp.com/api";
 
 function App() {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
   const data = useSelector((state) => state.data);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(updateData({ id: 1 }));
+    axios
+      .get(baseURL)
+      .then((response) => {
+        dispatch(updateData(response.data));
+        setLoading(false);
+      })
+      .catch((error) => setError(error), setLoading(false));
   }, []);
+
+  if (error) return <p>Errorr</p>;
+  if (!data) return <p>No data</p>;
 
   return (
     <div className="App">
-      {console.log(data)}
-      <p>Testing</p>
+      {loading && <p>Loading</p>}
+      {!loading && error && <p>An error has occured, consider, reloading</p>}
+      {data && !loading && <p>Data...</p>}
     </div>
   );
 }
